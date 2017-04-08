579c66d98f94e25c7a14a229dc79630722ec4b4c
n3k00n3 <3mm4@riseup.net>
2017-04-07 22:21:21 -0300

    xor cipher

--- ASSEMBLY/XOR.asm
+++ ASSEMBLY/XOR.asm
@@ -9,7 +9,7 @@
 [BITS 64]
 
 section .data
-    usageMessage: db  'usage: ./xor <message> <key>, 0x0a, 0x00
+    usageMessage: db  'usage: ./xor <message> <key>', 0x0a, 0x00
     lenMessage: equ $-usageMessage
 
 section .text
@@ -25,6 +25,54 @@ section .text
     mov [rbp-0x10], rsi       ;ARGV
     cmp edi, 0x03             ;ARGC == 3?
     jl  usage
+    jg  usage
+    
+    mov rdi, [rsi+0x08]       ;Second Pointer
+    mov rsi, [rsi+0x10]       ;Third Pointer
+    call stringLen
+    call xor
+    
+    jmp exit
+    
+    xor:
+        push rbp
+        mov rbp, rsp
+        
+        xor rax, rax
+
+        .xorCipher:
+            mov dl, byte [rdi+rax]
+            xor byte[rsi + rax], dl
+            inc rax
+            cmp rax, rcx
+            je .printMessage
+            jmp .xorCipher
+
+        .printMessage:
+            mov rax, 0x01
+            mov rdi, 0x01
+            mov rdx, rcx
+            syscall
+
+        mov rsp, rbp
+        pop rbp
+        ret
+    
+    stringLen:
+        push rbp
+        mov rbp, rsp
+        xor rcx, rcx
+        
+        .nextchar:
+            cmp byte[rdi+rcx * 1], 0x00
+            je  .exitStrlen
+            inc rcx
+            jmp .nextchar
+
+        .exitStrlen:
+            mov rsp, rbp
+            pop rbp
+            ret
 
     usage:
         mov rax, 0x01
